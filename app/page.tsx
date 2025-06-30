59ffbdfb002dcd0e059e83f30dbb37319d69bc67
KREZOO <krezoo.yt@gmail.com>
2025-06-30 11:50:25 +0300

    preparation v1.2

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,7 @@ import styles from './page.module.scss';
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <div className={styles.element}>
-          <div className={styles.content}>Lorem</div>
-        </div>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis
-        voluptates modi, dolore nulla quas aliquid ab repellendus voluptatem sit
-        sequi a quasi commodi ipsam labore sed rem reiciendis. Qui, quasi.
-      </main>
+      <main className={styles.main}></main>
     </div>
   );
 }
