26d7a7da22aa9c13387a40d4f805512bdb78c9dd
KREZOO <111351989+KREZOO@users.noreply.github.com>
2025-07-03 13:34:48 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,6 @@ import styles from './page.module.scss';
 export default function Home() {
   return (
     <div className={styles.page}>
-      <header
-        style={{ height: '60px', width: '100%', backgroundColor: '#fff' }}
-      ></header>
       <main className={styles.main}>
         <Hero />
       </main>
