1a8a08b01f0bea825198afb2f1d63cac07ea435c
KREZOO <krezoo.yt@gmail.com>
2025-07-04 17:04:50 +0300

    Created section numbers

---
+++ components/numbers/Numbers.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import { useIsMobile } from '@/hooks/useIsMobile';
+import { StaticCard } from './staticCard/StaticCard';
+import Style from './Numbers.module.scss';
+import Image from 'next/image';
+
+interface Props {}
+
+export function Numbers({}: Props) {
+  const isMobile = useIsMobile();
+
+  return (
+    <section className={Style.numbers}>
+      <Image
+        src={isMobile ? '/images/border-top-mb.png' : '/images/border-top.png'}
+        width={1550}
+        height={50}
+        alt='borderTop'
+        className='borderTop'
+      />
+
+      <div className={Style.bg} />
+
+      <div className={Style.content + ' container'}>
+        <header className={Style.header}>
+          <h3 className='h3'>Numbers</h3>
+
+          <div className={Style.desc + ' desc'}>Manage positions on</div>
+        </header>
+
+        <main className={Style.main}>
+          <div className={Style.titleWrapper}>
+            <div className={Style.title}>$18.5 B+</div>
+            <div className={Style.subtitle}>Total trading volume</div>
+          </div>
+
+          <div className={Style.stats}>
+            <div className={Style.statsTitle + ' desc'}>
+              Actual statistic on 19.07.2022
+            </div>
+
+            <div className={Style.statsCards}>
+              <StaticCard
+                data='36М+'
+                title='We are online'
+                className={Style.solo}
+              />
+              <StaticCard data='21.5 K+' title='Traiders in platform' />
+              <StaticCard data='325 M+' title='Total deals in platform' />
+            </div>
+          </div>
+        </main>
+      </div>
+    </section>
+  );
+}
